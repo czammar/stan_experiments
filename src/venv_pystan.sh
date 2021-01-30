221f73009622201d6b99fe5ab7efb6760cc378c5
Cesar Zamora Martinez <44296157+czammar@users.noreply.github.com>
2021-01-30 08:39:55 -0600

    Automatic activation comment

--- src/venv_pystan.sh
+++ src/venv_pystan.sh
@@ -3,8 +3,11 @@ KERNEL_NAME=custom_pystan
 PYTHON_VERSION=3.8.1
 
 pyenv install "$PYTHON_VERSION"
-
 pyenv virtualenv "$PYTHON_VERSION" "$KERNEL_NAME"
+pyenv activate "$KERNEL_NAME"
+
+# Uncomment for automatic active in directory
+# echo "$KERNEL_NAME" > .python-version
 
 # customize for packages installation
 pip install numpy scipy matplotlib seaborn openpyxl statsmodels scikit-learn pandas
